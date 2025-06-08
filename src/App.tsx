f0293690978fe7077725af17fd9743c06cbe5417
Annie Wu <annie25506@gmail.com>
2025-06-09 01:18:01 +0800

    chore: update app.tsx for router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
-import router from "@/router";
+import router from "@/router/Index";
 import "@/index.css";
 
 const App = () => {
